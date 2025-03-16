09341a422c8301b907ecb98f016057fa6d743e01
Gabriel Costa <gubsocosta@gmail.com>
2025-03-16 16:42:08 -0300

    refactor: rename component

--- src/components/BookProgress.vue
+++ src/components/BookProgress.vue
@@ -1,19 +1,22 @@
 <script setup>
-import {computed} from "vue";
+import {computed} from 'vue';
 
 const props = defineProps(['books']);
 const booksRead = computed(() => props.books.filter(b => b.isRead).length);
 const booksReadMessage = computed(() => booksRead.value >= props.books.length
-    ? 'Parabens, voce leu todos os livros'
-    : `${booksRead.value} de ${props.books.length} livro(s) lido(s)`
+  ? 'Parabens, voce leu todos os livros'
+  : `${booksRead.value} de ${props.books.length} livro(s) lido(s)`
 );
 </script>
 
 <template>
   <div class="books-read">
     <label for="progress">Seu Progresso</label>
-    <br />
-    <progress :value="booksRead" :max="books.length"></progress>
+    <br>
+    <progress
+      :value="booksRead"
+      :max="books.length"
+    />
     <p>{{ booksReadMessage }}</p>
   </div>
 </template>
